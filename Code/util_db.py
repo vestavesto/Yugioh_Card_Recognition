07c9d8ae4307b903badd4d1631038ed3cb830964
vestavesto <pretentious777@gmail.com>
2023-03-10 01:49:57 +0900

    update

--- Code/util_db.py
+++ Code/util_db.py
@@ -2,58 +2,52 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from scipy.stats import norm
 
 cwd = Path.cwd()
 dir_main = Path.cwd().parent
 
-dir_full_db = f'{dir_main}/Data/YGO DB - export_full_db.csv'
-df = pd.read_csv(dir_full_db)
-def fill_na_with_code(df):
+def load_db(bool):
+    if not bool:
+        return None
+    dir_full_db = f'{dir_main}/Data/YGO DB - export_full_db.csv'
+    df = pd.read_csv(dir_full_db)
+    df['Staple'].fillna('전용')
     for col in df.columns:
         if col != 'Code' and df[col].isnull().any():
             df[col].fillna(df['Code'], inplace=True)
+    mean = df['Usage'].mean()
+    std = df['Usage'].std()
+    stdev = df['Usage'].apply(lambda x: round(norm.cdf(x, mean, std),2))
+    df['SD'] = stdev
     return df
-fill_na_with_code(df)
+
+def add_sd(df):
+    mean = df['Usage'].mean()
+    std = df['Usage'].std()
+    stdev = df['Usage'].apply(lambda x: round(norm.cdf(x, mean, std),2))
+    df['SD'] = stdev
+    return df
+
+df = load_db(bool)
 db_digit = np.asarray(df["Digit"])
-db_name_ko = np.asarray(df["Name_KO"])
-db_type = np.asarray(df["Type"])
-
-# def search_deck(deck_code):
-#     dir_deck_img = f'{dir_main}/deck'
-#     deck_img_list = os.listdir(dir_deck_img)
-#     deck_code_list = [os.path.splitext(filename)[0] for filename in deck_img_list]
-#     deck_code_ind = deck_code_list.index(deck_code)
-#     deck_path = deck_img_list[deck_code_ind]
-#     return f'{dir_main}/deck/{deck_path}'
-
-# def search_deck(deck_code):
-#     dir_deck_img = f'{dir_main}/deck'
-#     dir_path = Path(dir_deck_img)
-
-#     files_list = list(dir_path.glob('*'))
-#     filenames_list = [str(file) for file in files_list]
-
-#     deck_code_list = [file.stem for file in dir_path.glob('*')]
-#     deck_code_ind = deck_code_list.index(deck_code)
-#     deck_path = filenames_list[deck_code_ind]
-
-#     return deck_path
-
 
 def search_deck(deck_code):
     dir_deck_img = Path(dir_main).joinpath('deck')
-
     # create a dictionary of deck codes and file paths
     deck_paths = {file.stem: str(file) for file in dir_deck_img.glob('*')}
-
     # get the deck path for the given deck code
     deck_path = deck_paths.get(deck_code)
-
     # return the deck path, or None if the deck code is not found
     return deck_path
 
 
-
-def search_card(card_name):
-    ind = np.where(db_name_ko == card_name)[0][0]
+def search_digit(card_name, local):
+    db_name = np.asarray(df[f"Name_{local.upper()}"])
+    ind = np.where(db_name == card_name)[0][0]
     return db_digit [ind]
+
+def search_name(digit, local):
+    db_name = np.asarray(df[f"Name_{local.upper()}"])
+    ind = np.where(db_digit == digit)[0][0]
+    return db_name [ind]
--- Code/util_trim.py
+++ Code/util_trim.py
@@ -1,9 +1,8 @@
-from copy import deepcopy
-# from util_tools import dist, sort_pt
 import numpy as np
 import pandas as pd
 from pathlib import Path
 import math
+from util_db import load_db
 
 cwd = Path.cwd()
 dir_main = Path.cwd().parent
@@ -21,7 +20,8 @@ def dist(p1, p2):
 
 # creates sorting mask funciton to sort points
 def sort_pt(pts, sort_round):
-    sort_up = 1000
+    numb = int(math.log10(sort_round))+2
+    sort_up = 10**numb
     ind_pt = []
     new_pt = []
     for i in range(len(pts)):
@@ -32,14 +32,7 @@ def sort_pt(pts, sort_round):
         new_pt.append(new)
     return [x for _, x in sorted(zip(new_pt, ind_pt))]
 
-dir_full_db = f'{dir_main}/Data/YGO DB - export_full_db.csv'
-df = pd.read_csv(dir_full_db)
-def fill_na_with_code(df):
-    for col in df.columns:
-        if col != 'Code' and df[col].isnull().any():
-            df[col].fillna(df['Code'], inplace=True)
-    return df
-fill_na_with_code(df)
+df = load_db(True)
 db_digit = np.asarray(df["Digit"])
 db_type = np.asarray(df["Type"])
 
@@ -54,8 +47,6 @@ def trans_raw ( card_loc, card_pos , digit_list):
         digit = digit_list[i]
         for j in range(len(card_loc[i])):
             coord = card_loc[i][j]
-            # coX = coord[0]
-            # coY = coord[1]
             pos = card_pos[i][j]
             # 01 Digit
             zip_digit.append(digit)
@@ -63,55 +54,50 @@ def trans_raw ( card_loc, card_pos , digit_list):
             zip_pt.append(coord)
             # 03 Pos
             zip_pos.append(pos)
-
     return zip_digit, zip_pt, zip_pos
 
 def trans_dup (zip_pt, dup_tol) :
-    dup_pt = deepcopy(zip_pt)
+    dup_pt = list(zip_pt) #deepcopy(zip_pt)
+    zip_ind = list(range(len(zip_pt)))
+    dup_ind = []
     i=0
     while i< len(dup_pt):
+        ind = [zip_ind[i]]
         j = len(dup_pt) -1
         while j > i:
             d = dist( dup_pt[i], dup_pt[j])
             if d < dup_tol **2 :
                 dup_pt.pop(j)
+                ind.append(zip_ind[j])
+                zip_ind.pop(j)
             j = j- 1
+        dup_ind.append(ind)
         i += 1
-    return dup_pt
+    return dup_pt, dup_ind
 
-# def trans_dup(zip_pt, dup_tol):
-#     dup_pt = deepcopy(zip_pt)
-#     dup_pt = [pt for i, pt in enumerate(dup_pt) if all(dist(pt, dup_pt[j]) >= dup_tol ** 2 or j >= i for j in range(len(dup_pt)))]
-#     return dup_pt
+# def trans_group (dup_pt, zip_pt, zip_pos, zip_digit, dup_tol) :
+#     group_pt = []
+#     group_pos = []
+#     group_digit = []
+#     for i in range(len(dup_pt)):
+#         pt_group = []
+#         pos_group = []
+#         digit_group = []
+#         for j in range(len(zip_pt)):
+#             d = dist(dup_pt[i], zip_pt[j])
+#             if d < dup_tol**2:
+#                 pt_group.append(zip_pt[j])
+#                 pos_group.append(zip_pos[j])
+#                 digit_group.append(zip_digit[j])
+#         group_pt.append(pt_group)
+#         group_pos.append(pos_group)
+#         group_digit.append(digit_group)
+#     return group_pt, group_pos, group_digit
 
-# def trams_dup(zip_pt, dup_tol):
-#     # Convert list of points to numpy array
-#     points = np.array(zip_pt)
-#     # Calculate pairwise distances
-#     dists = np.sqrt(np.sum((points[:, np.newaxis] - points) ** 2, axis=-1))
-#     # Create mask for points within tolerance
-#     mask = np.tril(dists <= dup_tol, k=-1).any(axis=1)
-#     # Return unique points
-#     return points[mask].tolist()
-
-def trans_group (dup_pt, zip_pt, zip_pos, zip_digit, dup_tol) :
-    group_pt = []
-    group_pos = []
-    group_digit = []
-    for i in range(len(dup_pt)):
-        pt_group = []
-        pos_group = []
-        digit_group = []
-        for j in range(len(zip_pt)):
-            d = dist(dup_pt[i], zip_pt[j])
-            if d < dup_tol**2:
-                pt_group.append(zip_pt[j])
-                pos_group.append(zip_pos[j])
-                digit_group.append(zip_digit[j])
-                
-        group_pt.append(pt_group)
-        group_pos.append(pos_group)
-        group_digit.append(digit_group)
+def trans_group(dup_ind, zip_pt, zip_pos, zip_digit):
+    group_pt = [[zip_pt[ind] for ind in sublist] for sublist in dup_ind]
+    group_pos = [[zip_pos[ind] for ind in sublist] for sublist in dup_ind]
+    group_digit = [[zip_digit[ind] for ind in sublist] for sublist in dup_ind]
     return group_pt, group_pos, group_digit
 
 def trans_sort (group_pos, group_digit, group_pt, sort_round):
@@ -131,7 +117,7 @@ def trans_name(sort_digit, local):
     for digit in sort_digit:
         ind = np.where(db_digit == int(digit))[0][0]
         name = np.asarray(df[f"Name_{local.upper()}"])[ind]
-        sort_name.append(name)
+        sort_name.append(str(name))
     return sort_name
 
 def trans_type(sort_digit):
